0d5295d032c8dad732402a68cc72b6c77e0764d6
GaganaGavesh <gaveshgamage@gmail.com>
2021-07-20 15:58:37 +0530

    Feature two done in feature 2 branch

--- src/Components/Unauthorized.js
+++ src/Components/Unauthorized.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 //This page will be viewed for all unauthorized routes
 //This page will be viewed inside protected route
 //This is dev branch change
+//Feature 2 done
 
 const Unauthorized = () => {
   return (
