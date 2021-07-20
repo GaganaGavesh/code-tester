dc3983e4c7325258e9fb3f84f7cbda833a931a06
GaganaGavesh <gaveshgamage@gmail.com>
2021-07-20 16:00:14 +0530

    Feature three done in feature branch

--- src/Components/Unauthorized.js
+++ src/Components/Unauthorized.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 
 //This page will be viewed for all unauthorized routes
 //This page will be viewed inside protected route
+//This feature branch change 3
 
 const Unauthorized = () => {
   return (
